94c60cd3c9fd619defc85ed799e1ecd4290cfbc8
Elayne Souza <elaynesouza@Elaynes-MacBook-Air.local>
2023-12-05 15:56:28 -0300

    Adding e2e tests with Playwright.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # googlesearch
+
+
+## Environment setup
+- npm install
+
+
+## Executing the test
+- npm test => To execute the test headless
+- npm run testui => To execute the test opening the modal page.
+- npm run report => To open the html report 
+
+
+
+## Test Strategy
+### Cross browser coverage:
+  The test is being executed in Chrome, Safari and Firefox.
+
+### Test coverage:
+  There are two test cases. One perform the searsh using the key search as a url parameter. The second test fill the search text field with the key and then execute the test openign the second result item in a new tab. 
+
+## Implementation
+
+Playwright framework is being used to implement the e2e test. For more information, please visit https://playwright.dev/docs. 
+
+I would like to inform that for this experiment I have decided to use Application Actions instead of Page Object Pattern, because we are not we are not using the same element locators across different scripts. For this reason, I am just using constants to keep the page test info clear and easy to maintain. For more information, please visit https://www.cypress.io/blog/2019/01/03/stop-using-page-objects-and-start-using-app-actions.
---
+++ tests/googleSearch.spec.js
@@ -0,0 +1,53 @@
+
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Google Search: Access the second result item in a new tab.', () => {
+
+  let SEARCH_KEY = 'selenium';
+  let URL_WITH_PARAMETER_KEY = `./search?q=${SEARCH_KEY}`;
+  let SEARCH_BUTTON_NAME = 'Google Search';
+  let GOOGLE_PAGE_TITLE = 'Google';
+  let GOOGLE_SEARCH_PAGE_TITLE_EN = `${SEARCH_KEY} - Google Search`;
+
+
+  test.beforeEach(async ({ page }) => {
+    test.slow();
+  });
+
+  test('Searching by search text field', async ({ page }) => {
+
+    await page.goto('./');
+    await page.getByRole('combobox').fill(SEARCH_KEY)
+    await page.getByRole('button').filter({ hasText: `${SEARCH_BUTTON_NAME}` }).click()
+
+    let elementText = await page.locator('a >> h3:visible').nth(1).textContent();
+
+    const [newtab] = await Promise.all([
+      page.context().waitForEvent('page'),
+      page.keyboard.press('Control'),
+      page.locator('a >> h3:visible').nth(1).click({ button: 'middle' })
+    ]);
+
+    expect(page).toHaveTitle(`${GOOGLE_SEARCH_PAGE_TITLE_EN}`);
+    expect(elementText).toContain(await newtab.title());
+
+  });
+
+  test('Searching by url parameter', async ({ page }) => {
+    await page.goto(URL_WITH_PARAMETER_KEY);
+    expect(page.url()).toContain('');
+    let elementText = await page.locator('a >> h3:visible').nth(1).textContent();
+
+    const [newtab] = await Promise.all([
+      page.context().waitForEvent('page'),
+      page.keyboard.press('Control'),
+      page.locator('a >> h3:visible').nth(1).click({ button: 'middle' })
+    ]);
+    
+    expect(page).toHaveTitle(`${GOOGLE_SEARCH_PAGE_TITLE_EN}`);
+    expect(elementText).toContain(await newtab.title());
+  });
+
+
+})
